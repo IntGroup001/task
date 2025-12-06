5862d9f93799a87538224724191eca9b14953c5f
DamyanBG <d.dimitrov.1988@abv.bg>
2025-12-06 08:34:46 +0200

    add main, routers, db and config

---
+++ backend/app/db.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+from config import settings
+
+engine = create_async_engine(
+    str(settings.SQLALCHEMY_DATABASE_URI),
+    pool_pre_ping=True,
+    pool_size=5,
+)
+SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+Base = declarative_base()
+
+
+async def get_db():
+    async with SessionLocal() as session:
+        yield session
